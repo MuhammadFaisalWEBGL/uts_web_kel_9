76284aa6e5454b153700fc098b8a139eca6b2df6
sttcabdul <abdul10222077@sttcipasung.ac.id>
2024-11-08 23:42:32 +0700

    =menambahkan responsif dan transisi pada tombol humberger

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-
-
 // buatElementHeader();
 function buatElementHeader() {
     document.getElementById('header').innerHTML = `
     <div class="header p-3 bg-gradient-to-r from-lime-100 to-green-500">
     <!-- bagian navbar -->
-            <nav class="flex capitalize gap-4 justify-between items-center">
-                <div class="flex items-center justify-between w-[35%]">
-                    <img src="../img/image.png" class="logo">
-                    <ul class="gap-3 flex">
+            <nav class="flex flex-col md:flex-row capitalize gap-4 justify-between items-center">
+                <div class="flex flex-col md:flex-row items-center justify-between w-full md:w-[35%]">
+                    <div class="flex items-center justify-between w-full md:w-auto">
+                        <img src="../img/image.png" class="logo mb-4 md:mb-0">
+                        <!-- Hamburger menu untuk mobile -->
+                        <button class="md:hidden text-2xl" id="menuToggle">
+                            <i class="bi bi-list"></i>
+                        </button>
+                    </div>
+                    
+                    <!-- Menu navigasi yang dapat di-toggle -->
+                    <ul class="hidden md:flex flex-col md:flex-row  gap-3 text-center w-full transition-all duration-300 ease-in-out" id="navMenu">
                         <li><a id="home" href="../home/index.html?aktif=home">home</a></li>
                         <li>
                             <div class="dropdown">
@@ -43,13 +49,11 @@ function buatElementHeader() {
                                 </button>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" id="verifikasi" href="../layanan/verifikasi.html?aktif=verifikasi">verifikasi ijazah</a></li>
-                        
                                     <li><a class="dropdown-item" id="jurnal" href="../layanan/jurnal.html?aktif=jurnal">jurnal</a></li>
-                                    
                                 </ul>
                             </div>
-                            </li>
-                            <li>
+                        </li>
+                        <li>
                             <div class="dropdown">
                                 <button class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                     Tautan
@@ -64,48 +68,46 @@ function buatElementHeader() {
                 </div>
                 
                 <!-- bagian pencarian -->
-                <div class="flex items-center gap-4">
-                <div class="flex justify-between rounded-md overflow-hidden">
-                <i class="bi p-1 bg-black cursor-pointer bi-search text-white"></i>
-                <input type="search" class="outline-none p-1" placeholder="cari">
-                </div>
-                <div class="cursor-pointer" id="kontak">kontak</div>
+                <div class="flex flex-col md:flex-row items-center gap-4 mt-4 md:mt-0">
+                    <div class="flex justify-between rounded-md overflow-hidden">
+                        <i class="bi p-1 bg-black cursor-pointer bi-search text-white"></i>
+                        <input type="search" class="outline-none p-1" placeholder="cari">
+                    </div>
+                    <div class="cursor-pointer" id="kontak">kontak</div>
                 
-                <div class="absolute bg-black/75 top-0 left-0 bottom-0 right-0 hidden z-20">
-                    <div class="bg-gray-100 flex items-center justify-center w-96 min-h-screen rounded-3xl top-7 left-[50%] translate-x-[-50%] absolute">
-                        <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                            <h1 class="text-2xl font-bold mb-6 text-center">Hubungi Admin</h1>
-                            <form action="mailto:admin@example.com" method="post" enctype="text/plain">
-                                <div class="mb-4">
-                                    <label for="name" class="block text-gray-700 font-bold mb-2">Nama:</label>
-                                    <input type="text" id="name" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-                                </div>
-                                <div class="mb-4">
-                                    <label for="email" class="block text-gray-700 font-bold mb-2">Email:</label>
-                                    <input type="email" id="email" name="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-                                </div>
-                                <div class="mb-4">
-                                    <label for="subject" class="block text-gray-700 font-bold mb-2">Subjek:</label>
-                                    <input type="text" id="subject" name="subject" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-                                </div>
-                                <div class="mb-4">
-                                    <label for="message" class="block text-gray-700 font-bold mb-2">Pesan:</label>
-                                    <textarea id="message" name="message" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required></textarea>
-                                </div>
-                                <div class="flex items-center justify-between">
-                                    <input type="submit" value="Kirim" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                </div>
-                            </form>
+                    <div class="absolute bg-black/75 top-0 left-0 bottom-0 right-0 hidden z-20">
+                        <div class="bg-gray-100 flex items-center justify-center w-full md:w-96 min-h-screen rounded-3xl top-7 left-[50%] translate-x-[-50%] absolute">
+                            <div class="bg-white p-4 md:p-8 rounded-lg shadow-lg w-full max-w-md mx-4">
+                                <h1 class="text-xl md:text-2xl font-bold mb-6 text-center">Hubungi Admin</h1>
+                                <form action="mailto:admin@example.com" method="post" enctype="text/plain">
+                                    <div class="mb-4">
+                                        <label for="name" class="block text-gray-700 font-bold mb-2">Nama:</label>
+                                        <input type="text" id="name" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="email" class="block text-gray-700 font-bold mb-2">Email:</label>
+                                        <input type="email" id="email" name="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="subject" class="block text-gray-700 font-bold mb-2">Subjek:</label>
+                                        <input type="text" id="subject" name="subject" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="message" class="block text-gray-700 font-bold mb-2">Pesan:</label>
+                                        <textarea id="message" name="message" rows="4" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required></textarea>
+                                    </div>
+                                    <div class="flex items-center justify-between">
+                                        <input type="submit" value="Kirim" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                 </div>
 
-
-                <div class="indo"></div>
+                    <div class="indo"></div>
                 </div>
             </nav>
         </div>
-    
     `
 }
 
@@ -127,63 +129,66 @@ function sorotTautanAktif() {
     if (tautanAktif) {
         tautanAktif.classList.add('aktif');
         if (induk.tagName.toLowerCase() !== 'img') {
-            // console.log('Induk elemen adalah <img>');
             induk.classList.add('aktif');
         }
     }
 }
 
 window.onload = function () {
-    // document.href.location = 'index.html?aktif=home'
     buatElementHeader();
     sorotTautanAktif();
     
-const kontak = document.querySelector('#kontak');
-kontak.addEventListener('click', e => {
-    e.target.nextElementSibling.classList.toggle('hidden');
-});
+    const kontak = document.querySelector('#kontak');
+    kontak.addEventListener('click', e => {
+        e.target.nextElementSibling.classList.toggle('hidden');
+    });
+
+    // Tambahkan event listener untuk toggle menu hamburger
+    const menuToggle = document.querySelector('#menuToggle');
+    const navMenu = document.querySelector('#navMenu');
+    
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('hidden');
+        navMenu.classList.toggle('opacity-0');
+        navMenu.classList.toggle('opacity-100');
+        navMenu.classList.toggle('scale-95');
+        navMenu.classList.toggle('scale-100');
+    });
 };
 
-
-
-
-
-
 document.querySelector('#footer').innerHTML = `
-    <div class="flex justify-center items-center p-10 bg-gray-700">
-        <div class="flex justigy-between gap-5 text-white  capitalize">
-            <div>
-                <ul>
-                    <li>tentang STTC</li>
+    <div class="flex justify-center items-center p-6 md:p-10 bg-gray-700">
+        <div class="flex flex-col md:flex-row justify-between gap-8 md:gap-12 text-white capitalize">
+            <div class="text-center md:text-left">
+                <ul class="space-y-2">
+                    <li class="font-bold mb-2">tentang STTC</li>
                     <li>Sejarah</li>
                     <li>Visi dan Misi</li>
                     <li>Struktur Organisasi</li>
                     <li>Tugas dan fungsi</li>
                 </ul>
             </div>
-            <div>
-                <ul>
-                    <li>penerimaan</li>
+            <div class="text-center md:text-left">
+                <ul class="space-y-2">
+                    <li class="font-bold mb-2">penerimaan</li>
                     <li>sarjana</li>
                     <li>pertukaran mahasiwa</li>
                 </ul>
             </div>
-            <div>
-                <ul>
-                    <li>pendidikan </li>
+            <div class="text-center md:text-left">
+                <ul class="space-y-2">
+                    <li class="font-bold mb-2">pendidikan</li>
                     <li>program studi</li>
                     <li>beasiswa</li>
                     <li>staf</li>
                 </ul>
             </div>
-            <div>
-                <ul>
-                    <li>kampus</li>
+            <div class="text-center md:text-left">
+                <ul class="space-y-2">
+                    <li class="font-bold mb-2">kampus</li>
                     <li>aktifitas mahasiswa</li>
                     <li>jelajah</li>
                 </ul>
             </div>
         </div>
     </div>`
-
-
